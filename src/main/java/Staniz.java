d82c5d5228172f0a95293f32594275411d7548a3
Jonathen Cheng <jonathencheng86@gmail.com>
2026-08-28 20:10:39 +0800

    Levl-2

--- src/main/java/Staniz.java
+++ src/main/java/Staniz.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Runs the Staniz personal assistant chatbot.
  */
 public class Staniz {
+    private static final String ADDED_MESSAGE_PREFIX = "added: ";
     private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
     private static final String BANNER = " ____ _____  _    _   _ ___ _____\n"
             + "/ ___|_   _|/ \\  | \\ | |_ _|__  /\n"
             + "\\___ \\ | | / _ \\ |  \\| || |  / /\n"
@@ -15,11 +19,13 @@ public class Staniz {
     private static final String FAREWELL = "Bye. Hope to see you again soon!";
 
     /**
-     * Starts Staniz and echoes user input until the exit command is entered.
+     * Starts Staniz and stores user entries until the exit command is entered.
      *
      * @param args command-line arguments; not used
      */
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
+
         System.out.println(BANNER);
         System.out.println(GREETING);
 
@@ -29,10 +35,27 @@ public class Staniz {
                 if (EXIT_COMMAND.equals(input)) {
                     break;
                 }
-                System.out.println(input);
+
+                if (LIST_COMMAND.equals(input)) {
+                    printTasks(tasks);
+                } else {
+                    tasks.add(input);
+                    System.out.println(ADDED_MESSAGE_PREFIX + input);
+                }
             }
         }
 
         System.out.println(FAREWELL);
+    }
+
+    /**
+     * Prints all stored tasks using one-based numbering.
+     *
+     * @param tasks tasks to display
+     */
+    private static void printTasks(List<String> tasks) {
+        for (int index = 0; index < tasks.size(); index++) {
+            System.out.printf("%d. %s%n", index + 1, tasks.get(index));
+        }
     }
 }
